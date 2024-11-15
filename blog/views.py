e664fc78f650f44104b85babb830e959543e75e5
Luisa <lopesluisa@usp.br>
2024-11-15 20:23:56 -0300

    Views funcionais sem Django Forms e sem validação

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.all().order_by('-date_posted')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_create(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        post = Post.objects.create(title=title, content=content)
+        return redirect('post-detail', pk=post.id)
+    return render(request, 'blog/post_form.html')
+
+def post_update(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        post.title = request.POST.get('title')
+        post.content = request.POST.get('content')
+        post.save()
+        return redirect('post-detail', pk=post.id)
+    return render(request, 'blog/post_form.html', {'post': post})
+
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('post-list')
+    return render(request, 'blog/post_confirm_delete.html', {'post': post})
