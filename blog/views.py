d974b5eba7639d3136cc48c0a6e581740f570d84
Luisa <lopesluisa@usp.br>
2024-11-15 20:43:50 -0300

    Views funcionais utilizando Django Forms

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from .forms import PostForm
 
 def post_list(request):
     posts = Post.objects.all().order_by('-date_posted')
@@ -9,22 +10,27 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+
 def post_create(request):
     if request.method == 'POST':
-        title = request.POST.get('title')
-        content = request.POST.get('content')
-        post = Post.objects.create(title=title, content=content)
-        return redirect('post-detail', pk=post.id)
-    return render(request, 'blog/post_form.html')
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('post-list')
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_form.html', {'form': form})
 
 def post_update(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
-        post.title = request.POST.get('title')
-        post.content = request.POST.get('content')
-        post.save()
-        return redirect('post-detail', pk=post.id)
-    return render(request, 'blog/post_form.html', {'post': post})
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('post-detail', pk=post.id)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_form.html', {'form': form})
 
 def post_delete(request, pk):
     post = get_object_or_404(Post, pk=pk)
